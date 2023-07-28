b2d747eb4c7773aa4efda6fb9d202a8e764f3d6f
Кошельников Владимир <vkoshelnikov@bk.ru>
2023-07-28 09:20:45 +0300

    First commit

---
+++ Bot.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unit11HW
+{
+    public class Bot
+    {
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeworkTelegramBot{
+    public class Program{
+        public static async Task Main(){
+            Console.WriteLine("Hello");
+        }
+    }
+}
